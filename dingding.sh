5741aff2658801088e464befdc24580d80ace2aa
837468220 <837468220@qq.com>
2020-11-16 08:28:16 +0800

    tab -> 2spaces

--- dingding.sh
+++ dingding.sh
@@ -2,7 +2,7 @@
 exec 1>>~/dingding/dingding.log 2>&1
 set -xv
 #-o表示短选项，两个冒号表示该选项有一个可选参数，可选参数必须紧贴选项
-#		如-carg 而不能是-c arg
+#    如-carg 而不能是-c arg
 #--long表示长选项
 #"$@" ：参数本身的列表，也不包括命令本身
 # -n:出错时的信息
@@ -14,8 +14,8 @@ SCKEY=
 MIPUSHID=
 TEMP=$(getopt -o -- --long 'mipushid::,sckey::' -n 'dingding.sh' -- "$@")
 if [ $? -ne 0 ]; then
-	echo 'Terminating...' >&2
-	exit 1
+  echo 'Terminating...' >&2
+  exit 1
 fi
 # Note the quotes around `$TEMP': they are essential!
 #set 会重新排列参数的顺序，也就是改变$1,$2...$n的值，这些值在getopt中重新排列过了
@@ -23,50 +23,50 @@ eval set -- "$TEMP"
 unset TEMP
 
 while true; do
-	case "$1" in
-		'--sckey')
-			case "$2" in
-				'')
-					echo 'Option sckey, no argument'
-				;;
-				*)
-					echo "Option sckey, argument '$2'"
-					SCKEY=$2
-				;;
-			esac
-			shift 2
-			continue
-		;;
-		'--mipushid')
-			# c has an optional argument. As we are in quoted mode,
-			# an empty parameter will be generated if its optional
-			# argument is not found.
-			case "$2" in
-				'')
-					echo 'Option mipushid, no argument'
-				;;
-				*)
-					echo "Option mipushid, argument '$2'"
+  case "$1" in
+    '--sckey')
+      case "$2" in
+        '')
+          echo 'Option sckey, no argument'
+        ;;
+        *)
+          echo "Option sckey, argument '$2'"
+          SCKEY=$2
+        ;;
+      esac
+      shift 2
+      continue
+    ;;
+    '--mipushid')
+      # c has an optional argument. As we are in quoted mode,
+      # an empty parameter will be generated if its optional
+      # argument is not found.
+      case "$2" in
+        '')
+          echo 'Option mipushid, no argument'
+        ;;
+        *)
+          echo "Option mipushid, argument '$2'"
           MIPUSHID=$2
-				;;
-			esac
-			shift 2
-			continue
-		;;
-		'--')
-			shift
-			break
-		;;
-		*)
-			echo 'Internal error!' >&2
-			exit 1
-		;;
-	esac
+        ;;
+      esac
+      shift 2
+      continue
+    ;;
+    '--')
+      shift
+      break
+    ;;
+    *)
+      echo 'Internal error!' >&2
+      exit 1
+    ;;
+  esac
 done
 
 echo 'Remaining arguments:'
 for arg; do
-	echo "--> '$arg'"
+  echo "--> '$arg'"
 done
     #curl "https://script.haokaikai.cn/MiPush/index.php?id=${MIPUSHID}&title=%E9%92%89%E9%92%89%E6%89%93%E5%8D%A1&msg=%E5%85%B3%E9%97%ADwifi%E6%89%93%E5%BC%80data" ;
 #curl "https://sc.ftqq.com/${SCKEY}.send?text=%e9%92%89%e9%92%89%e6%89%93%e5%8d%a1%e6%88%90%e5%8a%9f"
@@ -101,7 +101,7 @@ grep $md 2020_jiejiari.json && {
 }
 ping -W 2 -c 1 www.baidu.com && : || { adb -s $sn shell svc data enable ; adb -s $sn shell svc wifi disable ;
     if [[ -n ${MIPUSHID} ]] ; then
-			curl "https://script.haokaikai.cn/MiPush/index.php?id=${MIPUSHID}&title=%E9%92%89%E9%92%89%E6%89%93%E5%8D%A1&msg=%E5%85%B3%E9%97%ADwifi%E6%89%93%E5%BC%80data" ;
+      curl "https://script.haokaikai.cn/MiPush/index.php?id=${MIPUSHID}&title=%E9%92%89%E9%92%89%E6%89%93%E5%8D%A1&msg=%E5%85%B3%E9%97%ADwifi%E6%89%93%E5%BC%80data" ;
     fi
     if [[ -n ${SCKEY} ]] ; then
       curl "https://sc.ftqq.com/${SCKEY}.send?text=%E9%92%89%E9%92%89%E6%89%93%E5%8D%A1close%20wifi%20and%20open%20data"
